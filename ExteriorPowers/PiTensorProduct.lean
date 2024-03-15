6b2067caa884766337a90a8ac2a22e49e35c0257
morel <smorel@math.princeton.edu>
2024-03-15 09:57:02 +0100

    grmf

--- ExteriorPowers/PiTensorProduct.lean
+++ ExteriorPowers/PiTensorProduct.lean
@@ -12,7 +12,6 @@ variable {R₁ R₂ : Type*}
 variable {E : ι → Type*} [∀ i, SeminormedAddCommGroup (E i)] [∀ i, NormedSpace 𝕜 (E i)]
 
 variable {F : Type*} [SeminormedAddCommGroup F] [NormedSpace 𝕜 F]
-variable {G : Type*} [SeminormedAddCommGroup G] [NormedSpace 𝕜 F]
 
 open scoped TensorProduct
 
@@ -29,7 +28,7 @@ lemma toDualMultilinearMap_apply_tprod (m : (i : ι) → E i) (f : MultilinearMa
     toDualMultilinearMap (⨂ₜ[𝕜] i, m i) f = f m := by
   simp only [toDualMultilinearMap_apply_apply, lift.tprod]
 
-lemma toDualContinuousAlternatingMap_bound (x : ⨂[𝕜] i, E i) :
+lemma toDualMultilinearMap_bound (x : ⨂[𝕜] i, E i) :
     ∃ (C : ℝ), 0 ≤ C ∧ ∀ (f : ContinuousMultilinearMap 𝕜 E F),
     ‖toDualMultilinearMap x f.toMultilinearMap‖ ≤ C * ‖f‖ := by
   induction' x using PiTensorProduct.induction_on with r m x y hx hy
@@ -50,22 +49,66 @@ lemma toDualContinuousAlternatingMap_bound (x : ⨂[𝕜] i, E i) :
       rw [map_add, add_mul]
       refine le_trans (norm_add_le _ _) (add_le_add (hCx.2 f) (hCy.2 f))
 
-#exit
+@[simps!]
+noncomputable def toDualContinuousMultilinearMap : (⨂[𝕜] i, E i) →ₗ[𝕜]
+    ContinuousMultilinearMap 𝕜 E F →L[𝕜] F where
+  toFun x := {(toDualMultilinearMap x) ∘ₗ ContinuousMultilinearMap.toMultilinearMapLinear with
+    cont := by
+      obtain ⟨C, hC⟩ := toDualMultilinearMap_bound x (F := F)
+      simp only [AddHom.toFun_eq_coe, LinearMap.coe_toAddHom]
+      apply AddMonoidHomClass.continuous_of_bound _ C
+      intro f
+      simp only [LinearMap.coe_comp, Function.comp_apply,
+        ContinuousMultilinearMap.toMultilinearMapLinear_apply]
+      exact hC.2 f}
+  map_add' x y := by
+    ext f
+    simp only [map_add, ContinuousLinearMap.coe_mk', LinearMap.coe_comp, Function.comp_apply,
+      ContinuousMultilinearMap.toMultilinearMapLinear_apply, LinearMap.add_apply,
+      toDualMultilinearMap_apply_apply, ContinuousLinearMap.add_apply]
+  map_smul' a x := by
+    ext f
+    simp only [map_smul, ContinuousLinearMap.coe_mk', LinearMap.coe_comp, Function.comp_apply,
+      ContinuousMultilinearMap.toMultilinearMapLinear_apply, LinearMap.smul_apply,
+      toDualMultilinearMap_apply_apply, RingHom.id_apply, ContinuousLinearMap.coe_smul',
+      Pi.smul_apply]
 
+@[simp]
+lemma toDualContinuousMultilinearMap_apply_tprod (m : (i : ι) → E i)
+    (f : ContinuousMultilinearMap 𝕜 E F) :
+    toDualContinuousMultilinearMap (⨂ₜ[𝕜] i, m i) f = f m := by
+  simp only [toDualContinuousMultilinearMap_apply_toFun, lift.tprod,
+    ContinuousMultilinearMap.coe_coe]
 
-variable (𝕜 E r)
+lemma toDualMultilinearMap_bound' (x : ⨂[𝕜] i, E i) :
+    ∃ (C : ℝ), 0 ≤ C ∧ ∀ (G : Type*) [SeminormedAddCommGroup G]
+    [NormedSpace 𝕜 G] (f : ContinuousMultilinearMap 𝕜 E G),
+    ‖toDualMultilinearMap x f.toMultilinearMap‖ ≤ C * ‖f‖ := by
+  induction' x using PiTensorProduct.induction_on with r m x y hx hy
+  · existsi ‖r‖ * ∏ i : ι, ‖m i‖
+    constructor
+    · exact mul_nonneg (norm_nonneg r) (Finset.prod_nonneg (fun i _ ↦ norm_nonneg (m i)))
+    · intro _ _ _ f
+      simp only [map_smul, LinearMap.smul_apply, toDualMultilinearMap_apply_apply, lift.tprod,
+        ContinuousMultilinearMap.coe_coe, norm_smul]
+      rw [mul_assoc, mul_comm _ ‖f‖]
+      exact le_trans (mul_le_mul_of_nonneg_left (ContinuousMultilinearMap.le_opNorm f m)
+        (norm_nonneg r)) (le_refl _)
+  · obtain ⟨Cx, hCx⟩ := hx; obtain ⟨Cy, hCy⟩ := hy
+    existsi Cx + Cy
+    constructor
+    · exact add_nonneg hCx.1 hCy.1
+    · intro _ _ _ f
+      rw [map_add, add_mul]
+      refine le_trans (norm_add_le _ _) (add_le_add (hCx.2 _ f) (hCy.2 _ f))
 
-def toDualContinuousAlternatingMapLinear :
-    ⋀[𝕜]^r E →ₗ[𝕜] NormedSpace.Dual 𝕜 (E [⋀^(Fin r)]→L[𝕜] 𝕜) :=
-{
-  toFun := fun x => LinearMap.mkContinuousOfExistsBound (toDualContinuousAlternatingMap x)
-    (by obtain ⟨C, h⟩ := toDualContinuousAlternatingMap_bound x (F :=𝕜); existsi C; exact h.2)
-  map_add' := fun x y => by ext f; simp only [toDualContinuousAlternatingMap_add,
-    LinearMap.mkContinuousOfExistsBound_apply, LinearMap.add_apply, ContinuousLinearMap.add_apply]
-  map_smul' := fun a x => by ext f; simp only [toDualContinuousAlternatingMap_smul,
-    LinearMap.mkContinuousOfExistsBound_apply, LinearMap.smul_apply, smul_eq_mul, RingHom.id_apply,
-    ContinuousLinearMap.coe_smul', Pi.smul_apply]
-}
-
+noncomputable example : Seminorm 𝕜 (⨂[𝕜] i, E i) :=
+  Seminorm.comp
+  {toFun := fun x ↦ ‖x‖
+   map_zero' := by aesop
+   add_le' := fun x y ↦ by simp?
+   neg' := sorry
+   smul' := sorry}
+  (toDualContinuousMultilinearMap (F := F) (𝕜 := 𝕜) (E := E))
 
 end PiTensorProduct
